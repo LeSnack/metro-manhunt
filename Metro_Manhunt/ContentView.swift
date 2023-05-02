5d353e18481249d37a43f6273e1f57baadab1069
Zachary Walls <zacwalls20@gmail.com>
2023-05-02 16:22:51 +1000

    Update ContentView.swift

--- Metro_Manhunt/ContentView.swift
+++ Metro_Manhunt/ContentView.swift
@@ -6,21 +6,62 @@
 //
 
 import SwiftUI
+import CoreLocation
+import MapKit
 
 struct ContentView: View {
+    
+    @StateObject private var viewModel = ContentViewModel()
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -27.470125, longitude: 153.021072), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Map(coordinateRegion: $region, showsUserLocation: true)
+            .ignoresSafeArea()
+            .onAppear {
+                viewModel.checkIfLocationServicesIsEnabled()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+final class ContentViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager?
+    
+    func checkIfLocationServicesIsEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        } else {
+            print("Turn on Location Manager")
+        }
+    }
+    
+    private func checkLocationAuthorisation() {
+        guard let locationManager = locationManager else { return }
+        
+        switch locationManager.authorizationStatus {
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                print("Location Restricted")
+            case .denied:
+                print("Location Has Been Denied in Settings")
+            case .authorizedAlways,.authorizedWhenInUse:
+                break
+            @unknown default:
+                break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager) {
+        checkLocationAuthorisation()
     }
 }
