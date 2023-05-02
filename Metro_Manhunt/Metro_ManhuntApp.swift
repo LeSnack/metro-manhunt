f34062f20483cba9f6ebb809f40c17c613619145
Zachary Walls <zacwalls20@gmail.com>
2023-05-02 16:22:01 +1000

    Initial Commit

---
+++ Metro_Manhunt/Metro_ManhuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metro_ManhuntApp.swift
+//  Metro_Manhunt
+//
+//  Created by Zac Whales on 2/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Metro_ManhuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
